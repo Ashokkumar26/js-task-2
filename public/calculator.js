36cb226869b091c2c046f1ebc248ec0759eddbe1
Ashokkumar26 <koshakkumar3@gmail.com>
2019-12-31 13:01:33 +0530

    jstask-2

---
+++ public/calculator.js
@@ -0,0 +1,16 @@
+function update(value) {
+  //Type the code here.
+  document.myForm.screen.value += value;
+
+}
+function delet(value) {
+	document.myForm.screen.value = myForm.screen.value.substring(0,myForm.screen.value.length - 1);
+}
+function result(VALUE) {
+  document.myForm.screen.value = eval(document.myForm.screen.value);
+}
+
+function form_reset() {
+  //Type the code here.
+    document.myForm.screen.value = '';
+}
